470c36276f96dafc4f65454e57e81f33b966b8c3
Ivan <Korneychik91@yandex.ru>
2022-11-11 23:58:19 +0300

    second commit

---
+++ HomeWork/examples_54/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] getArray(int m, int n)        //////////заполнение двухмерного массива случайными числами.
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+    return array;
+}
+void printArray(int[,] array)                                    ///////////вывод двухмерного массива
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] sortElementArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int z = 0; z < array.GetLength(1) - 1; z++)
+            {
+                if (array[i, z] < array[i, z + 1])
+                {
+                    int temp = array[i, z + 1];
+                    array[i, z + 1] = array[i, z];
+                    array[i, z] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+//////////////////////////
+Console.Clear();
+int rows = new Random().Next(3, 6);
+int columns = new Random().Next(3, 6);
+int[,] array = getArray(rows, columns);
+Console.WriteLine("Сгенерированный массив");
+printArray(array);
+int[,] sortArray = sortElementArray(array);
+Console.WriteLine("\nМассив с упорядочными по убыванию элементами");
+printArray(sortArray);
---
+++ HomeWork/examples_56/Program.cs
@@ -0,0 +1,57 @@
+﻿//  Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+int[,] getArray(int m, int n)        //////////заполнение двухмерного массива случайными числами.
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+    return array;
+}
+void printArray(int[,] array)                                    ///////////вывод двухмерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void strokaMinSumElement(int[,] array)
+{
+    int sumMinStroki = 0;
+    int sumStroki = 0;
+    int StrokaMinSumEl = 0;
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sumMinStroki += array[0, j];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumStroki += array [i, j];
+            if (sumMinStroki > sumStroki)
+            {
+                sumMinStroki = sumStroki;
+                StrokaMinSumEl = i;
+            }
+            sumStroki = 0;
+        }
+    }
+    Console.Write ($"у {StrokaMinSumEl + 1} cтроки наименьшая сумма элементов ");
+}
+///////////////////////////////////
+Console.Clear();
+int rows = new Random().Next(3, 6);
+int columns = new Random().Next(3, 6);
+int[,] array = getArray(rows, columns);
+Console.WriteLine("Сгенерированный массив");
+printArray(array);
+strokaMinSumElement(array);
