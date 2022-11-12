0386c04a425743ba989c57465f6aedede986398b
Ivan <Korneychik91@yandex.ru>
2022-11-12 22:58:04 +0300

    3 commit

---
+++ HomeWork/examples_58/Program.cs
@@ -0,0 +1,72 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+int usernum(string Message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(Message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int usernum);
+        if (isCorrect)
+            return usernum;
+        Console.WriteLine(errorMessage);
+    }
+}
+int[,] getArray(int m, int n)        //////////заполнение двухмерного массива случайными числами.
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+    return array;
+}
+void printArray(int[,] array)                                    ///////////вывод двухмерного массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void resultArray(int[,] array, int[,] array2, int[,] array3)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            array3[i, j] = 0;
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                array3[i, j] += array[i, k] * array2[k, j];
+            }
+        }
+    }
+    Console.WriteLine("\nрезультирующая матрица ");
+    printArray(array3);
+}
+///////////////////////////////////
+Console.Clear();
+int rows = usernum("Введи количество строк: ", "Ошибка Ввода");
+int columns = usernum("Введи количество строк: ", "Ошибка Ввода");
+int[,] array = getArray(rows, columns);
+Console.WriteLine("\nСгенерированный массив 1");
+printArray(array);
+int[,] array2 = getArray(rows, columns);
+Console.WriteLine("\nСгенерированный массив 2");
+printArray(array2);
+int[,] array3 = new int[rows, columns];
+
+if (array.GetLength(0) != array2.GetLength(1))
+{
+    Console.Write("\nМатрицы нельзя перемножить");
+    return;
+}
+resultArray(array, array2, array3);
+
+
+
---
+++ HomeWork/examples_60/Program.cs
@@ -0,0 +1,41 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int[,,] getArray(int m, int n, int d)        //////////заполнение трехмерного массива случайными числами.
+{
+    int[,,] array = new int[m, n, d];
+    int count = 10;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int z = 0; z < d; z++)
+            {
+                array[i, j, z] +=count;
+                count +=1;
+            }
+        }
+    }
+    return array;
+}
+void printArray(int[,,] array)                                    ///////////вывод двухмерного массива
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                Console.Write($"{array[i, j, z]} ({i}, {j}, {z}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+///////////////////////////////////
+Console.Clear();
+int rows = new Random().Next(1, 4);
+int columns = new Random().Next(1, 4);
+int z = new Random().Next(1, 4);
+int[,,] array = getArray(rows, columns, z);
+Console.WriteLine("Сгенерированный массив");
+printArray(array);
